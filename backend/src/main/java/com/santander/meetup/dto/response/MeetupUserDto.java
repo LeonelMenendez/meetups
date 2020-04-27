7f624e56901d2aa69d07f05925d350c1ef7ca0d0
Leonel Menendez <leonelemenendez@gmail.com>
2020-04-27 02:18:00 -0300

    Angular setup

--- backend/src/main/java/com/santander/meetup/dto/response/MeetupUserDto.java
+++ backend/src/main/java/com/santander/meetup/dto/response/MeetupUserDto.java
@@ -1,7 +1,5 @@
 package com.santander.meetup.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Getter;
 import lombok.Setter;
 
